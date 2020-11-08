f763475b4505df9826f1bdb1466f57ffa9aebd8b
tayutaedomo <tayutaedomo@gmail.com>
2020-11-09 08:58:48 +0900

    Fix scripts/companies_export.js

--- scripts/companies_export.js
+++ scripts/companies_export.js
@@ -3,34 +3,53 @@ const puppeteer = require('puppeteer');
 const { JobCompanies } = require('../lib/company');
 
 
-const scraping = async () => {
-  const browser = await puppeteer.launch({
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox'
-    ]
-  });
+class PromiseSleeper {
+  constructor(msec) {
+    this.count = 0;
+    this.msec = msec || 15 * 1000;
+    this.setTimeout = ms => new Promise(resolve => setTimeout(resolve, ms));
+  }
 
-  const page = await browser.newPage();
+  async sleep() {
+    if (this.count == 0) {
+      this.count++;
 
-  const keyword = '機械学習';
-  const jobCompanies = new JobCompanies();
-  const companies = await jobCompanies.scrape(page, keyword, 1, 2);
-
-  await browser.close();
-
-  return companies;
-};
+    } else {
+      await this.setTimeout(this.msec);
+      this.count++;
+    }
+  }
+}
 
 
 if (require.main === module) {
   (async () => {
 
     try {
-      const companies = await scraping();
-      console.log(JSON.stringify(companies, null, 2));
+      const range = (start, end) => [...Array(end + 1).keys()].slice(start);
+      const sleeper = new PromiseSleeper(process.argv[2]);
+      const keyword = '機械学習';
 
-      // TODO: Export
+      const browser = await puppeteer.launch({
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ]
+      });
+
+      const page = await browser.newPage();
+
+      for await (const pageNum of range(1, 2)) {
+        await sleeper.sleep();
+
+        const jobCompanies = new JobCompanies();
+        const companies = await jobCompanies.scrape(page, keyword, pageNum);
+        console.log(JSON.stringify(companies, null, 2));
+
+        // TODO: Export
+      }
+
+      await browser.close();
 
     } catch(err) {
       console.error(err);
