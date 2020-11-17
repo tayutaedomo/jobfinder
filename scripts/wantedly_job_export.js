04495dcaf625cd2205e49f779ab296b546db7dd0
tayutaedomo <tayutaedomo@gmail.com>
2020-11-17 18:12:50 +0900

    Add pageNum handling to wantedly_job_export

--- scripts/wantedly_job_export.js
+++ scripts/wantedly_job_export.js
@@ -1,16 +1,19 @@
 /*
  * Usage:
- *    $ node scripts/wantedly_jobs_export.js
+ *    $ node scripts/wantedly_job_export.js <Interval> <First page num> <Last page num>
  */
 const puppeteer = require('puppeteer');
 
 const { WantedlyJobList } = require('../lib/wantedly_job');
+const { PromiseSleeper } = require('../lib/sleeper');
 
 
 if (require.main === module) {
   (async () => {
 
     try {
+      const range = (start, end) => [...Array(end + 1).keys()].slice(start);
+      const sleeper = new PromiseSleeper(process.argv[2]);
       const keyword = '機械学習';
 
       const browser = await puppeteer.launch({
@@ -19,13 +22,19 @@ if (require.main === module) {
           '--disable-setuid-sandbox'
         ]
       });
-
       const page = await browser.newPage();
 
-      const jobList = new WantedlyJobList();
-      const ret = await jobList.scrape(page, keyword);
-      console.log(jobList.jobs, ret);
-      console.log(ret);
+      const start = parseInt(process.argv[3] || 1);
+      const end = parseInt(process.argv[4] || 1);
+
+      for await (const pageNum of range(start, end)) {
+        await sleeper.sleep();
+
+        const jobList = new WantedlyJobList();
+        await jobList.scrape(page, keyword, pageNum);
+
+        console.log(jobList.jobs);
+      }
 
       await browser.close();
 
