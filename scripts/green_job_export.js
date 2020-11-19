1a88ee2a53dbc24bf756bec94104f798157cbd87
tayutaedomo <tayutaedomo@gmail.com>
2020-11-20 02:21:31 +0900

    Fix scraping/green_job_export.js

--- scripts/green_job_export.js
+++ scripts/green_job_export.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-const { GreenJobList } = require('../lib/green_job');
+const { GreenJobList, GreenJobWriter } = require('../lib/green_job');
 const { PromiseSleeper } = require('../lib/sleeper');
 
 
@@ -11,6 +11,7 @@ if (require.main === module) {
       const range = (start, end) => [...Array(end + 1).keys()].slice(start);
       const sleeper = new PromiseSleeper(process.argv[2]);
       const url = 'https://www.green-japan.com/search_key/01?key=6mfhgfcyuwglczadm660&keyword=機械学習';
+      const writer = new GreenJobWriter('green_jobs.csv');
 
       const browser = await puppeteer.launch({
         args: [
@@ -28,6 +29,8 @@ if (require.main === module) {
 
         const jobList = new GreenJobList(url);
         await jobList.scrape(page, pageNum);
+
+        await writer.append(jobList.jobs);
 
         console.log(jobList.jobs);
       }
