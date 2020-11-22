50d9d21c5149708f9b4dd6898c43967914594788
tayutaedomo <tayutaedomo@gmail.com>
2020-11-23 01:45:38 +0900

    Add scripts/paiza_job_export.js

---
+++ scripts/paiza_job_export.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer');
+
+const { PaizaJobList } = require('../lib/paiza_job');
+const { PromiseSleeper } = require('../lib/sleeper');
+
+
+if (require.main === module) {
+  (async () => {
+
+    try {
+      const range = (start, end) => [...Array(end + 1).keys()].slice(start);
+      const sleeper = new PromiseSleeper(process.argv[2]);
+
+      const browser = await puppeteer.launch({
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ]
+      });
+      const page = await browser.newPage();
+
+      const start = parseInt(process.argv[3] || 1);
+      const end = parseInt(process.argv[4] || 1);
+
+      for await (const pageNum of range(start, end)) {
+        await sleeper.sleep();
+
+        const jobList = new PaizaJobList();
+        await jobList.scrape(page, pageNum);
+
+        console.log(jobList.jobs);
+      }
+
+      await browser.close();
+
+    } catch(err) {
+      console.error(err);
+    }
+
+  })();
+}
