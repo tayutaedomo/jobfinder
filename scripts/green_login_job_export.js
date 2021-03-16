58ebf1bc6c09d1a81f5350d262ffd1a363c5147c
tayutaedomo <tayutaedomo@gmail.com>
2021-03-17 00:35:06 +0900

    add: search page handling

--- scripts/green_login_job_export.js
+++ scripts/green_login_job_export.js
@@ -1,28 +1,32 @@
 const puppeteer = require('puppeteer');
 
-const { GreenLogin } = require('../lib/green_login');
-//const { PromiseSleeper } = require('../lib/sleeper');
+const { GreenLogin, GreenSearch } = require('../lib/green_login');
 
 
 if (require.main === module) {
 
   (async () => {
 
-      const greenLogin = new GreenLogin(process.argv[2], process.argv[3]);
-      //const sleeper = new PromiseSleeper(process.argv[4]);
-      //const url = process.argv[5];
+    const greenLogin = new GreenLogin(process.argv[2], process.argv[3]);
+    const greenSearch = new GreenSearch(process.argv[4]);
 
     const browser = await puppeteer.launch({
       args: [
         '--no-sandbox',
-        '--disable-setuid-sandbox'
-      ]
+        '--disable-setuid-sandbox',
+        '--window-size=1024,640'
+      ],
+      headless: true,
+      defaultViewport: null,
     });
 
     try {
       const page = await browser.newPage();
 
       await greenLogin.login(page);
+      console.log('Logged in.');
+
+      await greenSearch.scraping(page);
 
       await browser.close();
 
